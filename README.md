2414de2b9c15cb333ec62786e4dfb0b82920bbc1
iabsensimaster <iabsensimaster@gmail.com>
2025-06-04 14:42:50 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ptestusaemandiriiledger
+# i-lejer
+ilejer
---
+++ config.js
@@ -0,0 +1,1 @@
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxF3YbZEl6eLDF85bNzDpzrGH8TjdNXGhr0flwTYaEohFCSrzvcWa-v5bduo0UOZcGj/exec";
